a9b429246a9f0cd823e10f38821c5a49948e50b6
SherlockHems <cjin0005@gmail.com>
2024-08-21 12:51:51 +0800

    Index htlm fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: '../dist',  // This will output the build to the root of your project
   },
 })
