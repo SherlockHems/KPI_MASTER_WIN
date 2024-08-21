8d97a0dcc39c88c0088d80f7233853b9827e6818
SherlockHems <cjin0005@gmail.com>
2024-08-21 13:16:07 +0800

    Index htlm fix3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, ''),
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   },
   server: {
     proxy: {
